0cc0c10cf6b36533cfd0d1a13b342fa06239514b
johansvedberg <johan@svedberg.biz>
2017-02-24 13:50:17 +0100

    Fixed text, and added some pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-$('select').material_select();
+    $('select').material_select();
 
     function checkTime(i) {
         return (i < 10) ? "0" + i : i;
@@ -16,5 +16,19 @@ $('select').material_select();
         }, 500);
     }
     startTime();
+
+
+
+    var cityData = [{city:"Redmond", population:57530},
+        {city:"Atlanta",population:447841},
+        {city:"San Fracisco", population:837442}];
+
+    alasql('CREATE INDEXEDDB DATABASE IF NOT EXISTS geo;\
+        ATTACH INDEXEDDB DATABASE geo; \
+        USE geo; \
+        DROP TABLE IF EXISTS cities; \
+        CREATE TABLE cities; \
+        SELECT * INTO cities FROM ?', [cityData], function(){
+    });
 
 });
